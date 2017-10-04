f0878d39bc1e40279af802cdc613f00b66056976
Dan Davis <daniel.davis@nih.gov>
2017-10-04 17:16:44 -0400

    Add username to config file and improve tests
    - username can now be specified as part of the configuration file
    - `nlmfedcred/cli.py` is refactored to be more testable
    - more tests are included now that `nlmfedcred\cli.py` is more testable

--- nlmfedcred/config.py
+++ nlmfedcred/config.py
@@ -2,11 +2,13 @@ from configparser import ConfigParser
 from collections import namedtuple
 import os
 
+__all__ = ('Config', 'parse_config', 'get_user', 'get_home',)
 
-Config = namedtuple('Config', ('account', 'role', 'idp',))
+
+Config = namedtuple('Config', ('account', 'role', 'idp', 'username',))
 
 
-def parse_config(profile, account, role, idp, inipath=None):
+def parse_config(profile, account, role, idp, username, inipath=None):
 
     if inipath is None:
         inipath = os.path.join(os.path.expanduser('~'), '.getawscreds')
@@ -30,4 +32,37 @@ def parse_config(profile, account, role, idp, inipath=None):
     if idp is None:
         idp = config.get(section, 'idp', fallback=None)
 
-    return Config(account, role, idp)
+    if username is None:
+        username = config.get(section, 'username', fallback=get_user())
+
+    return Config(account, role, idp, username)
+
+
+def get_user():
+    '''
+    Get the current username in a way that should work in Linux, OS X, and Windows
+    '''
+    if 'USER' in os.environ:
+        # shell in Linux/OS X
+        username = os.environ['USER']
+    elif 'USERNAME' in os.environ:
+        # shell in Windows
+        username = os.environ['USERNAME']
+    else:
+        # crontab in Linux/OS X, may not work with setuid/setgid programs.
+        # We assume a login shell changes semantics; this is by design
+        username = os.getlogin()
+    return username
+
+
+def get_home():
+    '''
+    Get the home directory of the user in a way that should work in Linux, OS X, and Windows
+    '''
+    if 'HOME' in os.environ:
+        home_path = os.environ['HOME']
+    elif 'HOMEDRIVE' in os.environ and 'HOMEPATH' in os.environ:
+        home_path = os.environ['HOMEDRIVE'] + os.environ['HOMEPATH']
+    else:
+        home_path = None
+    return home_path
