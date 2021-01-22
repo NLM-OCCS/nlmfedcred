1bdf836997b9521347946ce9ba328e3156cde25b
Dan Davis <daniel.davis@nih.gov>
2021-01-22 13:07:45 -0500

    Update idp URLs to use CertAuthV3 for PIV login and support a full url for idp

--- nlmfedcred/idp.py
+++ nlmfedcred/idp.py
@@ -1,11 +1,12 @@
 from collections import namedtuple
+from urllib.parse import urlsplit, urlunsplit
 
-FORM_URL_FORMAT = 'https://%s/affwebservices/public/saml2sso?SPID=urn:amazon:webservices&appname=NLM'
+FORM_URL_PATH = '/affwebservices/public/saml2sso'
+FORM_URL_QUERY = 'SPID=urn:amazon:webservices&appname=NLM'
 LOGIN_URL_FORMAT = 'https://%s/siteminderagent/forms/login.fcc'
-PIV_URL_FORMAT = 'https://piv%s/CertAuthV2/forms/HHSPIVRedirector.aspx'
-
+PIV_URL_FORMAT = 'https://%s/CertAuthV3/forms/NIHPIVRedirector.aspx'
 DEFAULT_IDP_FQDN = 'authtest.nih.gov'
-DEFAULT_FORM_URL = FORM_URL_FORMAT % DEFAULT_IDP_FQDN
+DEFAULT_FORM_URL = 'https://%s%s?%s' % (DEFAULT_IDP_FQDN, FORM_URL_PATH, FORM_URL_QUERY)
 DEFAULT_LOGIN_URL = LOGIN_URL_FORMAT % DEFAULT_IDP_FQDN
 DEFAULT_PIV_URL = PIV_URL_FORMAT % DEFAULT_IDP_FQDN
 
@@ -16,4 +17,13 @@ DEFAULT_IDP = IDP(DEFAULT_FORM_URL, DEFAULT_LOGIN_URL, DEFAULT_PIV_URL)
 
 
 def make_idp(fqdn):
-    return IDP(FORM_URL_FORMAT % fqdn, LOGIN_URL_FORMAT % fqdn, PIV_URL_FORMAT % fqdn)
+    if fqdn.startswith('https'):
+        url = urlsplit(fqdn)
+        fqdn = url.netloc
+        path = url.path if url.path else FORM_URL_PATH
+        query = url.query if url.query else FORM_URL_QUERY
+    else:
+        path = FORM_URL_PATH
+        query = FORM_URL_QUERY
+    form_url = urlunsplit(['https', fqdn, path, query, ''])
+    return IDP(form_url, LOGIN_URL_FORMAT % fqdn, PIV_URL_FORMAT % fqdn)
--- nlmfedcred/tests/test_idp.py
+++ nlmfedcred/tests/test_idp.py
@@ -5,11 +5,19 @@ from nlmfedcred.idp import (DEFAULT_FORM_URL, DEFAULT_LOGIN_URL,
 def test_defaults():
     assert 'authtest.nih.gov' in DEFAULT_FORM_URL
     assert 'authtest.nih.gov' in DEFAULT_LOGIN_URL
-    assert 'pivauthtest.nih.gov' in DEFAULT_PIV_URL
+    assert 'authtest.nih.gov' in DEFAULT_PIV_URL
 
 
 def test_make_idp():
     idp = make_idp('authfu.nih.gov')
     assert 'authfu.nih.gov' in idp.form_url
     assert 'authfu.nih.gov' in idp.login_url
-    assert 'pivauthfu.nih.gov' in idp.piv_url
+    assert 'authfu.nih.gov' in idp.piv_url
+
+
+def test_make_idp_from_url():
+    form_url = 'https://authfu.nih.gov/affweb/public/saml2sso?SPID=urn:amazon:webservices&appname=NHLBI'
+    idp = make_idp(form_url)
+    assert idp.form_url == form_url
+    assert 'authfu.nih.gov' in idp.login_url
+    assert 'authfu.nih.gov' in idp.piv_url
